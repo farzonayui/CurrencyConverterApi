eb41e3b51c9d69e32cbd554a67e8633068251f27
farzonayui <farzonakhon2018@gmail.com>
2026-03-25 15:16:48 +0500

    fix enum

---
+++ CurrencyConverterApi/Controllers/ConversionHistoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CurrencyConverterApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyConverterApi.Controllers
+{
+    [ApiController]
+    [Route("api/conversion-history")]
+    public class ConversionHistoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ConversionHistoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var history = _context.ConvertionHistories
+                .OrderByDescending(x => x.ConvertedAt)
+                .ToList();
+
+            return Ok(history);
+        }
+    }
+}
--- CurrencyConverterApi/Models/CurrencyCode.cs
+++ CurrencyConverterApi/Models/CurrencyCode.cs
@@ -2,13 +2,10 @@
 {
     public enum CurrencyCode
     {
-        USD,
-        EUR,
-<<<<<<< HEAD
-        RUB,
-        TJS
-=======
-        RUB
->>>>>>> 7171e30d519d0e7aac53b75d111be0cdb73de1b3
+       USD,
+       EUR,
+       TJS,
+       RUB
+
     }
 }
